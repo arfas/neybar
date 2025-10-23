e01251fdd2f0457236a712ddf055eb5bae41587c
arfas <arfaadeebmohammed@gmail.com>
2025-10-22 21:21:28 -0700

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders the main heading', () => {
-  render(<App />);
-  const headingElement = screen.getByText(/Meet Your Real Neighbors/i);
-  expect(headingElement).toBeInTheDocument();
-});
